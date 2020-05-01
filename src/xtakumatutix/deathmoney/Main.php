6888af341a8324b000e7bda0245228a589a68e57
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2020-05-02 07:50:52 +0900

    インデントひどかったのを修正

--- src/xtakumatutix/deathmoney/Main.php
+++ src/xtakumatutix/deathmoney/Main.php
@@ -13,9 +13,9 @@ use metowa1227\moneysystem\api\core\API;
 
 class Main extends PluginBase implements Listener{
 
-	public function onEnable(){
-		$this->getlogger()->info("読み込み完了");
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+    public function onEnable(){
+        $this->getlogger()->info("読み込み完了");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->Config = new Config($this->getDataFolder() . "Config.yml", Config::YAML, array(
             'メッセージ' => '§a死亡したので、{Money}円を失いました',
             'お金が足りない場合' => '§a死亡しましたが、お金が足りないので、ペナルティーとして0円にセットされました。',
@@ -25,18 +25,18 @@ class Main extends PluginBase implements Listener{
 
     public function onDeacth(PlayerDeathEvent $event)
     {
-    	$player = $event->getPlayer();
-    	$mymoney = API::getInstance()->get($player);
-    	$money = $this->Config->get("料金");
-    	$nomoney = $this->Config->get("お金が足りない場合");
-    	$message = $this->Config->get("メッセージ");
-    	$message = str_replace("{Money}", "{$money}", $message);
-    	if($mymoney < $money){
-    		$player->sendMessage("{$nomoney}");
-    		API::getInstance()->set($player,0);
-    	}else{
-    		$player->sendMessage("{$message}");
-    		API::getInstance()->reduce($player,$money);
-    	}
+        $player = $event->getPlayer();
+        $mymoney = API::getInstance()->get($player);
+        $money = $this->Config->get("料金");
+        $nomoney = $this->Config->get("お金が足りない場合");
+        $message = $this->Config->get("メッセージ");
+        $message = str_replace("{Money}", "{$money}", $message);
+        if($mymoney < $money){
+            $player->sendMessage("{$nomoney}");
+            API::getInstance()->set($player,0);
+        }else{
+            $player->sendMessage("{$message}");
+            API::getInstance()->reduce($player,$money);
+        }
     }
 }
